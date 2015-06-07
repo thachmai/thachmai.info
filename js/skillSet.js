a25649452b310e4014aaee29142c4d3093107409
Thach MAI <thachmai@gmail.com>
2015-06-07 18:59:14 +0200

    Adds better styling for skill set.

--- js/skillSet.js
+++ js/skillSet.js
@@ -1,6 +1,7 @@
 function skillSet() {
     'use strict';
 
+    var name = { 'front': 'Frontend', 'back': 'Backend', 'ops': 'DevOps', 'bus': 'Domain' };
     var skills = [];
     var selection = [];
     var width = 0;
@@ -24,15 +25,18 @@ function skillSet() {
         var binding = dThis.selectAll('.group').data(groups);
         var gEnter = binding.enter().append('g').attr('class', 'group');
 
-        gEnter.append('text').attr('x', groupX)
-            .attr('y', 20).text(function (d) { return d; });
+        gEnter.append('text').attr('x', groupX).attr('class', 'skill-group')
+            .attr('style', 'font-variant: small-caps;')
+            .attr('y', 20).text(function (d) { return name[d]; });
 
         gEnter.each(function (group, column) {
-            console.log(group, column)
                 var gSkills = skills.filter(function (s) { return s.group === group; });
             d3.select(this).selectAll('.skill-selection').data(gSkills)
                 .enter()
                 .append('text').attr('class', 'skill-selection')
+                .classed('selected', function (d) {
+                    return selection.indexOf(d.id) > -1;
+                })
                 .attr('style', 'font-size: 12px; opacity: 0;')
                 .text(function (d) { return d.sname; })
                 .attr('x', function (d, row) { return groupX(d, column); })
@@ -51,6 +55,13 @@ function skillSet() {
             return acc;
         }, d3.set()).values();
 
+        return me;
+    };
+
+    me.selection = function (value) {
+        if (!value) return selection;
+
+        selection = value;
         return me;
     };
 
